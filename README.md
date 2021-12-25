5ad8944e00c243c39eb98f3c7c1f76de8a232149
Nakayama Daiju <42.daiju@gmail.com>
2021-12-25 17:16:10 +0900

    mnistの推論速度測れるようになった

--- README.md
+++ README.md
@@ -7,18 +7,11 @@ PyTorch(tch-rc) → ONNX → Some ONNX runtime → wasm → Predict in browse
 ## onnx 動いてる
 
 ```
-$ cargo run --bin=mnist-onnx
+$ cargo run --bin=mnist-onnx --release
    Compiling torch-rust-practice v0.1.0 (/Users/daiju/ghq/github.com/hinohi/torch-rust-practice)
-    Finished dev [unoptimized + debuginfo] target(s) in 1.90s
-     Running `target/debug/mnist-onnx`
-6 81.17%
-5 12.41%
-0 6.41%
-3 0.00%
-9 0.00%
-8 0.00%
-4 0.00%
-1 0.00%
-7 0.00%
-2 0.00%
+    Finished release [optimized] target(s) in 1.71s
+     Running `target/release/mnist-onnx`
+load onnx model: 0.023sec
+load mnist test data: 0.035sec
+run: samples=10000 correct=9930 correct_rate=99.30% 2.624sec
 ```
--- src/bin/mnist-onnx.rs
+++ src/bin/mnist-onnx.rs
@@ -1,69 +1,91 @@
-use tract_core::prelude::*;
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+    time::Instant,
+};
+
 use tract_onnx::prelude::*;
 
-static DATA_6: &str = r"
-0000000000000000000000000000
-0000000000000000000000000000
-0000000000000011100000000000
-0000000000000111000000000000
-0000000000001110000000000000
-0000000000011100000000000000
-0000000001110000000000000000
-0000000111000000000000000000
-0000011110000000000000000000
-0000111000000000000000000000
-0001111000000000000000000000
-0001111001111111000000000000
-0001111111100001111000000000
-0001111000000000001110000000
-0001111000000000000011100000
-0001111000000000000001110000
-0000111000000000000001110000
-0000111000000000000001110000
-0000011100000000000001100000
-0000011100000000000111100000
-0000001110000000001111000000
-0000000011111111111000000000
-0000000000011111000000000000
-0000000000000000000000000000
-0000000000000000000000000000
-0000000000000000000000000000
-0000000000000000000000000000
-0000000000000000000000000000
-";
+fn read_u32<T: Read>(reader: &mut T) -> std::io::Result<u32> {
+    let mut b = [0u8; 4];
+    reader.read_exact(&mut b)?;
+    Ok(u32::from_be_bytes(b))
+}
 
-fn as_array(date: &str) -> tract_ndarray::Array4<f32> {
-    let mut v = Vec::with_capacity(28 * 28);
-    for c in date.bytes() {
-        match c {
-            b'1' => v.push(1.0),
-            b'0' => v.push(0.0),
-            _ => (),
-        }
-    }
-    tract_ndarray::Array4::from_shape_vec((1, 1, 28, 28), v).unwrap()
+fn read_images<P: AsRef<Path>>(path: P) -> std::io::Result<Tensor> {
+    let mut buf_reader = BufReader::new(File::open(path)?);
+    assert_eq!(read_u32(&mut buf_reader)?, 2051);
+    let samples = read_u32(&mut buf_reader)? as usize;
+    let rows = read_u32(&mut buf_reader)? as usize;
+    let cols = read_u32(&mut buf_reader)? as usize;
+    let data_len = samples * rows * cols;
+    let mut data = vec![0u8; data_len];
+    buf_reader.read_exact(&mut data).expect("Fail to read");
+    let arr = Tensor::from_shape(
+        &[samples, 1, 28, 28],
+        &data
+            .into_iter()
+            .map(|b| b as f32 / 255.0)
+            .collect::<Vec<f32>>(),
+    )
+    .unwrap();
+    Ok(arr)
+}
+
+fn read_labels<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<u8>> {
+    let mut buf_reader = BufReader::new(File::open(path)?);
+    assert_eq!(read_u32(&mut buf_reader)?, 2049);
+    let samples = read_u32(&mut buf_reader)? as usize;
+    let mut data = vec![0u8; samples];
+    buf_reader.read_exact(&mut data)?;
+    Ok(data)
 }
 
 fn main() -> TractResult<()> {
+    let start = Instant::now();
     let model = tract_onnx::onnx()
         .model_for_path("mnist.onnx")?
         .with_input_fact(
             0,
-            InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 1, 28, 28)),
+            InferenceFact::dt_shape(f32::datum_type(), [1, 1, 28, 28]),
         )?
         .into_optimized()?
         .into_runnable()?;
-    let input: Tensor = as_array(DATA_6).into();
-    let result = model.run(tvec!(input))?;
-    let mut result: Vec<(f32, usize)> = result[0]
-        .to_array_view()?
-        .iter()
-        .cloned()
-        .zip(0..)
-        .collect::<Vec<_>>();
-    result.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
-    for (p, i) in result {
-        println!("{} {:.2}%", i, p * 100.0);
+    println!("load onnx model: {:.3}sec", start.elapsed().as_secs_f64());
+
+    let start = Instant::now();
+    let images = read_images("data/t10k-images-idx3-ubyte").unwrap();
+    let labels = read_labels("data/t10k-labels-idx1-ubyte").unwrap();
+    println!(
+        "load mnist test data: {:.3}sec",
+        start.elapsed().as_secs_f64()
+    );
+
+    let start = Instant::now();
+    let mut samples = 0;
+    let mut correct = 0;
+    for i in 0..images.shape()[0] {
+        let image = images.slice(0, i, i + 1).unwrap();
+        let result = model.run(tvec!(image)).unwrap();
+        let (_, label) = result[0]
+            .to_array_view::<f32>()?
+            .iter()
+            .cloned()
+            .zip(0..)
+            .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+            .unwrap();
+        if label == labels[i] {
+            correct += 1;
+        }
+        samples += 1;
     }
+    println!(
+        "run: samples={} correct={} correct_rate={:.2}% {:.3}sec",
+        samples,
+        correct,
+        correct as f64 / samples as f64 * 100.0,
+        start.elapsed().as_secs_f64()
+    );
     Ok(())
 }
